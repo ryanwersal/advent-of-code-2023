86ec3fd4194fc44382d79b20381e27bce7f2ec7f
Ryan Wersal <ryan@ryanwersal.com>
2023-12-02 16:32:25 -0700

    Day 1 part 1 solution

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,3 +1,9 @@
 defmodule Day1 do
-
+  def calibrate(input) do
+    matches = Enum.flat_map(Regex.scan(~r/\d/, input), & &1)
+    [first] = Enum.take(matches, 1)
+    [last] = Enum.take(matches, -1)
+    {val, _} = Integer.parse("#{first}#{last}")
+    val
+  end
 end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -1,8 +1,21 @@
 defmodule Day1Test do
   use ExUnit.Case
-  doctest Day1
 
-  test "oi" do
-    assert "oi"
+  test "samples" do
+    assert Day1.calibrate("1abc2") == 12
+    assert Day1.calibrate("pqr3stu8vwx") == 38
+    assert Day1.calibrate("a1b2c3d4e5f") == 15
+    assert Day1.calibrate("treb7uchet") == 77
+  end
+
+  test "my input" do
+    results =
+      File.read!("inputs/day1.txt")
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.map(&Day1.calibrate(&1))
+      |> Enum.sum()
+
+    assert results != nil
   end
 end
