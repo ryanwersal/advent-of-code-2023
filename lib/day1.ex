c165397b5dace3d2dcaaea5d2c13066d129a6f98
Ryan Wersal <ryan@ryanwersal.com>
2023-12-02 19:59:04 -0700

    Day 1 part 2 solution

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,9 +1,44 @@
 defmodule Day1 do
-  def calibrate(input) do
-    matches = Enum.flat_map(Regex.scan(~r/\d/, input), & &1)
-    [first] = Enum.take(matches, 1)
-    [last] = Enum.take(matches, -1)
-    {val, _} = Integer.parse("#{first}#{last}")
-    val
+  def calibrate(input, fun) do
+    input |> String.trim() |> String.split("\n") |> Enum.map(&fun.(&1)) |> Enum.sum()
+  end
+
+  def calibrate_line(input) do
+    matches = Regex.scan(~r/\d/, input) |> Enum.flat_map(& &1)
+    [[first], [last]] = [Enum.take(matches, 1), Enum.take(matches, -1)]
+    "#{first}#{last}" |> String.to_integer()
+  end
+
+  @forwardRegex ~r/(?:\d|one|two|three|four|five|six|seven|eight|nine)/
+  @backwardRegex ~r/(?:\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)/
+
+  def calibrate_line2(input) do
+    first =
+      Regex.scan(@forwardRegex, input) |> Enum.flat_map(& &1) |> Enum.take(1) |> List.first()
+
+    last =
+      Regex.scan(@backwardRegex, String.reverse(input))
+      |> Enum.flat_map(& &1)
+      |> Enum.take(1)
+      |> List.first()
+      |> String.reverse()
+
+    "#{word_to_int(first)}#{word_to_int(last)}" |> String.to_integer()
+  end
+
+  @wordToInt %{
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9
+  }
+
+  defp word_to_int(word) do
+    Map.get(@wordToInt, word, word)
   end
 end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -2,20 +2,64 @@ defmodule Day1Test do
   use ExUnit.Case
 
   test "samples" do
-    assert Day1.calibrate("1abc2") == 12
-    assert Day1.calibrate("pqr3stu8vwx") == 38
-    assert Day1.calibrate("a1b2c3d4e5f") == 15
-    assert Day1.calibrate("treb7uchet") == 77
+    assert Day1.calibrate_line("1abc2") == 12
+    assert Day1.calibrate_line("pqr3stu8vwx") == 38
+    assert Day1.calibrate_line("a1b2c3d4e5f") == 15
+    assert Day1.calibrate_line("treb7uchet") == 77
+  end
+
+  test "sample sum" do
+    result =
+      "1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet"
+      |> Day1.calibrate(&Day1.calibrate_line(&1))
+
+    assert result == 142
   end
 
   test "my input" do
     results =
       File.read!("inputs/day1.txt")
-      |> String.trim()
-      |> String.split("\n")
-      |> Enum.map(&Day1.calibrate(&1))
-      |> Enum.sum()
+      |> Day1.calibrate(&Day1.calibrate_line(&1))
 
-    assert results != nil
+    assert results == 54239
+  end
+
+  test "part2 samples" do
+    assert Day1.calibrate_line2("two1nine") == 29
+    assert Day1.calibrate_line2("eightwothree") == 83
+    assert Day1.calibrate_line2("abcone2threexyz") == 13
+    assert Day1.calibrate_line2("xtwone3four") == 24
+    assert Day1.calibrate_line2("4nineeightseven2") == 42
+    assert Day1.calibrate_line2("zoneight234") == 14
+    assert Day1.calibrate_line2("7pqrstsixteen") == 76
+  end
+
+  test "part2 samples sum" do
+    result =
+      "two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen"
+      |> Day1.calibrate(&Day1.calibrate_line2(&1))
+
+    assert result == 281
+  end
+
+  test "part2 more samples" do
+    assert Day1.calibrate_line2("nineight") == 98
+  end
+
+  test "part2 my input" do
+    results =
+      File.read!("inputs/day1.txt")
+      |> Day1.calibrate(&Day1.calibrate_line2(&1))
+
+    assert results == 55343
   end
 end
